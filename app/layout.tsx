e07b813e4c18fac1495541afca30ffb5a1ed4d30
Илья Лаврентьев <ironfenomen@gmail.com>
2026-08-26 13:40:45 +0300

    sync core 1.0.4 (a11y: focus-visible ring + reduced-motion guard) + css v20260826a

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,10 +97,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             инлайн-critical из <head>, бандл догружает below-fold после load
             (flip в INIT_HOME). ?v= бампить при каждой пересборке (npm run css:split). */}
         {/* eslint-disable-next-line @next/next/no-css-tags */}
-        <link rel="stylesheet" href="/redesign/a26-bundle.min.css?v=20260825b" media="print" data-defer-css="1" />
+        <link rel="stylesheet" href="/redesign/a26-bundle.min.css?v=20260826a" media="print" data-defer-css="1" />
         <noscript>
           {/* eslint-disable-next-line @next/next/no-css-tags */}
-          <link rel="stylesheet" href="/redesign/a26-bundle.min.css?v=20260825b" />
+          <link rel="stylesheet" href="/redesign/a26-bundle.min.css?v=20260826a" />
         </noscript>
         <noscript><div><img src="https://mc.yandex.ru/watch/91506218" style={{ position: "absolute", left: "-9999px" }} alt="" /></div></noscript>
 
